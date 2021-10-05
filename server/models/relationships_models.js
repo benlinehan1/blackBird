0556ad5c0aa5fda3ebde814b69b0991d1363be81
Benjamin Linehan <benjaminlinehan21@gmail.com>
2021-10-06 08:57:08 +1000

    relationship model setup

--- server/models/relationships_models.js
+++ server/models/relationships_models.js
@@ -1,1 +1,52 @@
 const dbquery = require("../lib/dbQuery.js");
+
+function all() {
+	let sql = `
+	select * from relationships;`;
+
+	return dbquery(sql).then((dbRes) => {
+		return dbRes.rows;
+	});
+}
+
+function create(patient_id, doctor_id) {
+	let sql = `INSERT INTO relationships (patient_id, doctor_id) values ($1, $2) returning *`;
+
+	return dbquery(sql, [patient_id, doctor_id]).then((dbRes) => {
+		return dbRes.rows[0];
+	});
+}
+
+function pending(id) {
+	let sql = `SELECT pending FROM relationships WHERE id = $1;`;
+
+	return dbquery(sql, [id]).then((dbRes) => {
+		return dbRes.rows[0];
+	});
+}
+
+function confirm(id) {
+	let pending = pending(id);
+
+	if (pending === false) {
+		console.log("in progress");
+	} else {
+		return (sql = `UPDATE relationships SET pending = '${pending}'`);
+	}
+}
+
+function remove(id) {
+	let sql = `delete from relationships where id = $1;`;
+
+	return dbquery(sql, [id]).then((dbRes) => {
+		return dbRes.rows;
+	});
+}
+
+module.exports = {
+	all,
+	create,
+	pending,
+	confirm,
+	remove,
+};

8a31121255e8d418e83e44a60b5808c40973b157
Keegan Pratt <pratt.keegan@gmail.com>
2021-10-05 21:30:20 +1100

    Added date helper

---
+++ server/lib/dateHelp.js
@@ -0,0 +1,13 @@
+//This returns us the current date in the format of
+function newISO() {
+	return new Date().toISOString();
+}
+
+function formateDate(date) {
+	return date.toLocaleDateString("en-gb", {
+		year: "numeric",
+		month: "long",
+		day: "numeric",
+		timeZone: "AEDT",
+	});
+}

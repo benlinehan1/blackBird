497f02f8fde6c90f35e55162712c2da21ccd5d4d
Nicolas <nicolasngj@gmail.com>
2021-10-05 23:59:26 +1100

    comment

--- db_console.js
+++ db_console.js
@@ -1,9 +1,8 @@
-const test = require("./server/models/doctor_models.js");
+const test = require("./server/models/comments_models.js");
 
 let id = 1;
-let fullName = "Nic";
-let type = "psic";
-let credential = 1234;
+let pt = 2;
+let content = "Melhorou";
 
-test.doctorCreate(id, fullName, type, credential);
-console.log(test.doctorGetAllById(1));
+test.commentsCreate(id, pt, content);
+console.log(test.commentsGetAllByConsultation(2));

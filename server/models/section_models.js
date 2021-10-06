c9537d63a5438849a74c5431fe9dd94c0209af5f
Nicolas <nicolasngj@gmail.com>
2021-10-06 13:00:45 +1100

    section

--- server/models/section_models.js
+++ server/models/section_models.js
@@ -1,1 +1,16 @@
-console.log("bussin");
+const { dbQuery } = require("../lib/dbQuery.js");
+
+function sectionCreate(consultationId, title, content) {
+	let sql = "insert into section (consultation_id, title, content) values ($1,$2,$3);";
+	return dbQuery(sql, [consultationId, title, content]);
+}
+
+function sectionGetByConsulId(consultationId) {
+	let sql = "select * from section where consultation_id = $1;";
+	return dbQuery(sql, [consultationId]);
+}
+
+module.exports = {
+	sectionCreate,
+	sectionGetByConsulId,
+};

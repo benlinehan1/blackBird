f0f303277bfc99f49d1181e8d796b86031538f7e
Keegan Pratt <pratt.keegan@gmail.com>
2021-10-06 14:03:09 +1100

    added expirery date to new iso

--- server/lib/dateHelp.js
+++ server/lib/dateHelp.js
@@ -1,6 +1,12 @@
+const expireHours = 2;
+
 //This returns us the current date in the format of
 function newISO() {
 	return new Date().toISOString();
+}
+
+function newISOWithExpire() {
+	new Date(new Date().getTime() + expireHours * 60 * 60 * 1000);
 }
 
 function formateDate(date) {
@@ -15,4 +21,5 @@ function formateDate(date) {
 module.exports = {
 	newISO,
 	formateDate,
+	newISOWithExpire,
 };

366561eec4bd3b0ba08fddbfdefbabc50de9001b
Benjamin Linehan <benjaminlinehan21@gmail.com>
2021-10-06 14:05:52 +1000

    super sexy new components

--- client/views/scripts/patientview.js
+++ client/views/scripts/patientview.js
@@ -5,3 +5,9 @@ var sidebar = document.querySelector(".sidebar_patient");
 component("DoctorUl", { Class: "consultation-form" }).then((comp) => {
 	sidebar.appendChild(comp);
 });
+
+var patientConsultationDiv = document.querySelector(".consultations_patient");
+
+component("PatientBaseConsult", { Title: "Consultation", Date: "Placeholder" }).then((comp) => {
+	patientConsultationDiv.appendChild(comp);
+});
--- client/views/scripts/script.js
+++
@@ -1,7 +0,0 @@
-import component from "./../../lib/cmpParse.js";
-
-component("HPAddCon", {});
-
-component("Heading", { VarName: "hello", text: "bye" }).then((comp) => {
-	document.body.appendChild(comp);
-});
